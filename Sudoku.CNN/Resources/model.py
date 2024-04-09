02979254dd4e4de79ce50b6ad29e8e7b839fcb18
Florian <floriansegard@gmail.com>
2024-04-09 21:03:23 +0200

    model that was used

--- Sudoku.CNN/Resources/model.py
+++ Sudoku.CNN/Resources/model.py
@@ -24,27 +24,24 @@ def get_complete_model():
     input_layer = Input(shape=(9, 9, 1))
 
     # Base convolutional layers
-    conv_base1 = Conv2D(256, kernel_size=(3,3), activation='relu', padding='same')(input_layer)
+    conv_base1 = Conv2D(128, kernel_size=(3,3), activation='relu', padding='same')(input_layer)
     conv_base1 = BatchNormalization()(conv_base1)
-    conv_base1 = Dropout(0.40)(conv_base1)
-    conv_base2 = Conv2D(256, kernel_size=(3,3), activation='relu', padding='same')(conv_base1)
+    conv_base2 = Conv2D(128, kernel_size=(3,3), activation='relu', padding='same')(conv_base1)
     conv_base2 = BatchNormalization()(conv_base2)
-    conv_base2 = Dropout(0.40)(conv_base2)
-    conv_base3 = Conv2D(512, kernel_size=(1,1), activation='relu', padding='same')(conv_base2)
-    conv_base3 = BatchNormalization()(conv_base3)  # Ensure normalization after each conv layer
-    conv_base3 = Dropout(0.40)(conv_base3)
-
+    conv_base3 = Conv2D(256, kernel_size=(1,1), activation='relu', padding='same')(conv_base2)
+    conv_base3 = BatchNormalization()(conv_base3)
+    
     # Specialized convolutional layers for Sudoku rules, each followed by BatchNormalization
     # 3x3 convolutions with 3x3 strides for boxes
-    conv_boxes = Conv2D(512, (3, 3), strides=(3, 3), activation='relu', padding='same')(input_layer)
+    conv_boxes = Conv2D(128, (3, 3), strides=(3, 3), activation='relu', padding='same')(input_layer)
     conv_boxes = BatchNormalization()(conv_boxes)
 
     # 1x9 convolutions for rows
-    conv_rows = Conv2D(512, (1, 9), strides=(1, 1), activation='relu', padding='valid')(input_layer)
+    conv_rows = Conv2D(128, (1, 9), strides=(1, 1), activation='relu', padding='valid')(input_layer)
     conv_rows = BatchNormalization()(conv_rows)
 
     # 9x1 convolutions for columns
-    conv_columns = Conv2D(512, (9, 1), strides=(1, 1), activation='relu', padding='valid')(input_layer)
+    conv_columns = Conv2D(128, (9, 1), strides=(1, 1), activation='relu', padding='valid')(input_layer)
     conv_columns = BatchNormalization()(conv_columns)
 
     # Flatten and concatenate features from all paths
