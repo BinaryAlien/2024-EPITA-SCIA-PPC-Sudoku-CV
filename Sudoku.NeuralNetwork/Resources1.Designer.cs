ecaaeb5e0813e4db113fd48393d4c57a7bfc8479
theophile.stourbe <theophile.stourbe@epita.fr>
2024-04-10 11:17:28 +0200

    final commit

--- Sudoku.NeuralNetwork/Resources1.Designer.cs
+++ Sudoku.NeuralNetwork/Resources1.Designer.cs
@@ -81,7 +81,58 @@ namespace Sudoku.NeuralNetwork {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to PK!µçú‰??        ///metadata.json{&quot;keras_version&quot;: &quot;3.2.0&quot;, &quot;date_saved&quot;: &quot;2024-04-09@20:26:32&quot;}PK!&gt;Îžxjjconfig.json{&quot;module&quot;: &quot;keras&quot;, &quot;class_name&quot;: &quot;Sequential&quot;, &quot;config&quot;: {&quot;name&quot;: &quot;sequential&quot;, &quot;trainable&quot;: true, &quot;dtype&quot;: &quot;float32&quot;, &quot;layers&quot;: [{&quot;module&quot;: &quot;keras.layers&quot;, &quot;class_name&quot;: &quot;InputLayer&quot;, &quot;config&quot;: {&quot;batch_shape&quot;: [null, 9, 9, 1], &quot;dtype&quot;: &quot;float32&quot;, &quot;sparse&quot;: false, &quot;name&quot;: &quot;input_layer&quot;}, &quot;registered_name&quot;: null}, {&quot;module&quot;: &quot;keras.layers&quot;, &quot;class_name&quot;: &quot;Conv2D&quot; [rest of string was truncated]&quot;;.
+        ///   Looks up a localized string similar to import os
+        ///
+        ///import numpy as np
+        ///from keras import models, utils
+        ///
+        ///
+        ///class SudokuSolver:
+        ///    def __init__(self, model_path):
+        ///        self.model = self.load_model(model_path)
+        ///
+        ///    def load_model(self, model_path):
+        ///        return models.load_model(model_path)
+        ///
+        ///    def __call__(self, puzzles):
+        ///        puzzles = puzzles.copy()
+        ///        for _ in range((puzzles == 0).sum((1, 2)).max()):
+        ///            model_preds = self.model.predict(
+        ///                utils.to_categorical(puzzles, num_classes=10), verbose= [rest of string was truncated]&quot;;.
+        /// </summary>
+        internal static string solve_cnn_py {
+            get {
+                return ResourceManager.GetString("solve_cnn.py", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to import copy
+        ///import keras
+        ///import numpy as np
+        ///
+        ///
+        ///def norm(a):
+        ///    return (a/9)-.5
+        ///
+        ///def denorm(a):
+        ///    return (a+.5)*9
+        ///
+        ///def inference_sudoku(sample):
+        ///    
+        ///    &apos;&apos;&apos;
+        ///        This function solve the sudoku by filling blank positions one by one.
+        ///    &apos;&apos;&apos;
+        ///    
+        ///    feat = copy.copy(sample)
+        ///    
+        ///    while(1):
+        ///    
+        ///        out = model.predict(feat.reshape((1,9,9,1)))  
+        ///        out = out.squeeze()
+        ///
+        ///        pred = np.argmax(out, axis=1).reshape((9,9))+1 
+        ///        prob = np.around(np.max(out, axis=1) [rest of string was truncated]&quot;;.
         /// </summary>
         internal static string solve_convolutional_py {
             get {
